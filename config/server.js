2816d62cfbfb1c27f97d46da7db57b92d657497c
Taylor <tdy721@gmail.com>
2020-11-18 18:31:18 -0700

    upgrade

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '614257ce1696c813a5cdb591446b6d92'),
+      secret: env("ADMIN_JWT_SECRET", "614257ce1696c813a5cdb591446b6d92"),
     },
   },
-  url: 'https://api.videopoker.academy'
+  url: env("STRAPI_URL", "https://api.videopoker.academy"),
 });
