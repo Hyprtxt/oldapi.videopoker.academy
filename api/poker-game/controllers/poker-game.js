2e2c65d18c57f4b9e21fa372db3bf8ae3993fea5
Taylor <tdy721@gmail.com>
2020-11-18 18:43:51 -0700

    ok

--- api/poker-game/controllers/poker-game.js
+++ api/poker-game/controllers/poker-game.js
@@ -1,8 +1,26 @@
-'use strict';
-
+"use strict";
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+  async new(ctx) {
+    console.log(ctx, "MAKIN A GAME");
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.restaurant.create(data, { files });
+    } else {
+      entity = await strapi.services.restaurant.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.restaurant });
+  },
+};
