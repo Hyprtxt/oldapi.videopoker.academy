457618eb7c1ee8c9a86103d25ec28870129088dd
Taylor <taylor@eightfold.io>
2020-11-19 01:22:10 +0000

    ok

--- api/poker-game/controllers/poker-game.js
+++ api/poker-game/controllers/poker-game.js
@@ -1,8 +1,39 @@
 'use strict';
-
+const { sanitizeEntity } = require('strapi-utils');
 /**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
+ * A set of functions called "actions" for `poker-game`
  */
 
-module.exports = {};
+module.exports = {
+  // find: async (ctx) => {
+  //   let entities;
+  //   console.log(ctx.state.user)
+  //   if (ctx.query._q) {
+  //     entities = await strapi.services['poker-game'].search(ctx.query);
+  //   } else {
+  //     entities = await strapi.services['poker-game'].find(ctx.query);
+  //   }
+  //   // console.log(entities)
+  //   return entities.map(entity => {
+  //     console.log( entity )
+  //     // if ( entity.created_by.id === ctx.state.user.id ) {
+  //       return sanitizeEntity(entity, { model: strapi.models['poker-game'] })
+  //     // }
+  //   });
+  // },
+  myGames: async (ctx, next) => {
+    console.log( 'myGames', ctx, ctx.state );
+    try {
+      // ctx.body = 'ok';
+      let entities = await strapi.services['poker-game'].find(ctx.query);
+      return entities.map(entity => {
+        // console.log( entity )
+        if ( entity.User.id === ctx.state.user.id ) {
+          return sanitizeEntity(entity, { model: strapi.models['poker-game'] })
+        }
+      });
+    } catch (err) {
+      ctx.body = err;
+    }
+  }
+};
