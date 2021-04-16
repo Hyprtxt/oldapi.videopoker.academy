fb73d271f845871292effb1cac2a95450c8eee43
Taylor <taylor@eightfold.io>
2021-04-16 10:42:01 -0700

    ok

--- api/report/models/report.js
+++ api/report/models/report.js
@@ -4,10 +4,12 @@ const uuid = require("uuid")
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
+const SEVEN_DAYS_MS = 7 * 24 * 60 * 60 * 1000
 
 module.exports = {
   lifecycles: {
     beforeCreate: data => {
+      data.expires = new Date().getTime() + SEVEN_DAYS_MS
       data.uuid = uuid.v4()
     },
   },
