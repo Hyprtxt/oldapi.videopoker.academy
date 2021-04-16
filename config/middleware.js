43bb590655f7843310e8b3c2d9da20cbb7c4d52f
Taylor <taylor@eightfold.io>
2021-04-16 19:53:09 +0000

    cors

--- config/middleware.js
+++ config/middleware.js
@@ -5,6 +5,7 @@ module.exports = {
         "https://api.videopoker.academy",
         "https://videopoker.academy",
         "http://localhost:8000",
+        "http://localhost:8080",
         "http://localhost:1337",
       ],
     },
